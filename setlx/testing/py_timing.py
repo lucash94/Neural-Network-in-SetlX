c6945d50356ff00f67a1c02e8eaa1dfeebeceb39
Lucas Heuser <Lucas Heuser>
2017-03-15 21:17:38 +0100

    fixed performance testing

--- setlx/testing/py_timing.py
+++ setlx/testing/py_timing.py
@@ -1,7 +1,7 @@
 import numpy as np
 from time import time
 
-m = np.zeros( (1000,1000) )
+m = np.random.random((1000, 1000))
 
 print "Hadamard:"
 mSum = 0
@@ -17,7 +17,17 @@ print "\nMatrizen:"
 mSum = 0
 for i in range(10):
     startTime = time()
-    np.zeros( (1000,1000) )
+    np.random.random((1000, 1000))
+    endTime = time() - startTime
+    print str(i+1) + ". Runde:\t" + str(endTime * 1000) + "ms"
+    mSum += endTime * 1000
+print "Durchschnitt:\t" + str(mSum/10) + "ms"
+
+print "\nMatrizen-Multiplikation:"
+mSum = 0
+for i in range(10):
+    startTime = time()
+    res = m*m
     endTime = time() - startTime
     print str(i+1) + ". Runde:\t" + str(endTime * 1000) + "ms"
     mSum += endTime * 1000
